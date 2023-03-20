9e93f1e7b546631ea418a309f680d99612c6458f
SvenBudak <sven@budak.info>
2023-03-20 12:55:01 +0100

    3 files modified

--- src/app/features/feature-wallet/feature-wallet-overview/feature-wallet-overview.module.ts
+++ src/app/features/feature-wallet/feature-wallet-overview/feature-wallet-overview.module.ts
@@ -1,11 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 
 import { FeatureWalletOverviewComponent } from './feature-wallet-overview.component';
 import { FeatureWalletOverviewRoutingModule } from './feature-wallet-overview-routing.module';
 
 @NgModule({
   declarations: [FeatureWalletOverviewComponent],
-  imports: [CommonModule, FeatureWalletOverviewRoutingModule],
+  imports: [
+    CommonModule,
+    FeatureWalletOverviewRoutingModule,
+    IonicModule.forRoot(),
+  ],
 })
 export class FeatureWalletOverviewModule {}
--- src/app/features/feature-wallet/feature-wallet.module.ts
+++ src/app/features/feature-wallet/feature-wallet.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 
 import { FeatureWalletComponent } from './feature-wallet.component';
 import { FeatureWalletRoutingModule } from './feature-wallet-routing.module';
 
 @NgModule({
   declarations: [FeatureWalletComponent],
-  imports: [CommonModule, FeatureWalletRoutingModule],
+  imports: [CommonModule, FeatureWalletRoutingModule, IonicModule.forRoot()],
 })
 export class FeatureWalletModule {}
