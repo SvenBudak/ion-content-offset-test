364f947c6099bdc903c67e0e3f022ddbb476233f
SvenBudak <sven@budak.info>
2023-03-20 12:59:07 +0100

    2 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,11 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
+    redirectTo: 'wallet',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
     children: [
       {
         path: 'wallet',
